f95861050ba5c4c1082279a5520343706edef14a
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-01 12:41:56 -0700

    add seed data and json page for event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,18 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
+
+    respond_to do |format|
+      format.html {
+        render
+      }
+      format.json {
+        render json: @events
+      }
+    end
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 end
