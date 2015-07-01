ee381f9ddf6ec3bb21e426a6ca710ba955399e0b
Busyra Anwar <busyra@me.com>
2015-07-01 12:13:05 -0700

    adds yelp gem and initializer

---
+++ config/initializers/yelp.rb
@@ -0,0 +1,6 @@
+Yelp.client.configure do |config|
+  config.consumer_key = Wfc-m5mTQq2rUaBaoC046A
+  config.consumer_secret = -8qoL9X_6nFJKHuNwtpJL5WObEY
+  config.token = cFLhMqp-kgI5g-FgAw6yaLS4pYgftlUA
+  config.token_secret = jCpt-42G2voqHMoUfsjMB-WUD6M
+end
